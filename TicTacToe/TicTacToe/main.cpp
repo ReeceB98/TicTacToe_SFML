cd6d5ac856f4ba657af64a395510e97dd2925d28
Reece <reecebellchambers98@gmail.com>
2024-02-21 02:15:00 +0000

    Commented any code that needed understanding.
    Commented the small program that will be used for the next steps of the project such as the game class.

--- TicTacToe/TicTacToe/main.cpp
+++ TicTacToe/TicTacToe/main.cpp
@@ -2,22 +2,33 @@
 #include "SFML/Graphics.hpp"
 
 int main()
-{
+{   
+    // Declare and create a new render window
     sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+
+    // Declare and creates a green circle
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
 
+    // The game loop
     while (window.isOpen())
     {
+        // Processed events
         sf::Event event;
         while (window.pollEvent(event))
         {
+            // Request for closing the window
             if (event.type == sf::Event::Closed)
                 window.close();
         }
 
+        // Clear the window before rendering next frame
         window.clear();
+
+        // Render the green circle
         window.draw(shape);
+
+        // End the current frame and display the contents on screen
         window.display();
     }
 
