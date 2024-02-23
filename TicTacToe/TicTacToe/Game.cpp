5776f02880e2cc967443742835f35446ff843834
Reece <reecebellchambers98@gmail.com>
2024-02-23 00:22:26 +0000

    CircleShape bug is fixed.
    Needed to place the circle shape after the delta time variable. Not sure why but it works.

--- TicTacToe/TicTacToe/Game.cpp
+++ TicTacToe/TicTacToe/Game.cpp
@@ -1,15 +1,15 @@
 #include "Game.h"
 
 const sf::Time Game::TimePerFrame = sf::seconds(1.0f / 60.0f);
-
+sf::CircleShape shape(100.0f);
 Game::Game()
 : mWindow(sf::VideoMode(640, 480), "TicTacToe"){}
 
 void Game::Run()
-{
+{	
 	sf::Clock clock;
 	sf::Time timeSinceLastUpdate = sf::Time::Zero;
-
+	
 	while (mWindow.isOpen())
 	{
 		timeSinceLastUpdate += clock.restart();
@@ -26,7 +26,48 @@ void Game::Run()
 void Game::processEvents()
 {
 	sf::Event event{};
-	// CONTINUE HERE! //
+
+	while (mWindow.pollEvent(event))
+	{
+		switch (event.type)
+		{
+		case sf::Event::KeyPressed:
+			handleInputs(event.key.code, true);
+			break;
+		case sf::Event::KeyReleased:
+			handleInputs(event.key.code, false);
+			break;
+		case sf::Event::Closed:
+			mWindow.close();
+			break;
+		default:
+			break;
+		}
+	}
 }
+
+void Game::update(sf::Time deltaTime)
+{
+	// Update objects here
+	shape.setFillColor(sf::Color::Green);
+}
+
+void Game::render()
+{
+	mWindow.clear();
+
+	// Draw objects here
+	mWindow.draw(shape);
+
+	mWindow.display();
+}
+
+void Game::handleInputs(sf::Keyboard::Key key, bool isPressed)
+{
+	// key events for the game
+}
+
+
+
 
 
--- TicTacToe/TicTacToe/main.cpp
+++ TicTacToe/TicTacToe/main.cpp
@@ -1,36 +1,38 @@
 #include <iostream>
-#include "SFML/Graphics.hpp"
+#include "Game.h"
 
 int main()
 {   
-    // Declare and create a new render window
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    Game game;
+    game.Run();
+    //// Declare and create a new render window
+    //sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
 
-    // Declare and creates a green circle
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    //// Declare and creates a green circle
+    //sf::CircleShape shape(100.f);
+    //shape.setFillColor(sf::Color::Green);
 
-    // The game loop
-    while (window.isOpen())
-    {
-        // Processed events
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            // Request for closing the window
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
+    //// The game loop
+    //while (window.isOpen())
+    //{
+    //    // Processed events
+    //    sf::Event event;
+    //    while (window.pollEvent(event))
+    //    {
+    //        // Request for closing the window
+    //        if (event.type == sf::Event::Closed)
+    //            window.close();
+    //    }
 
-        // Clear the window before rendering next frame
-        window.clear();
+    //    // Clear the window before rendering next frame
+    //    window.clear();
 
-        // Render the green circle
-        window.draw(shape);
+    //    // Render the green circle
+    //    window.draw(shape);
 
-        // End the current frame and display the contents on screen
-        window.display();
-    }
+    //    // End the current frame and display the contents on screen
+    //    window.display();
+    //}
 
 	return 0;
 }
